71c2a2e9b94b3787de40a60ffce65770e8ba6f24
Michael Tambornino <michael.tambornino@gmail.com>
2016-12-12 16:01:51 -0600

    load and parse json

--- quiz.js
+++ quiz.js
@@ -1,35 +1,32 @@
-var inventory = [];
-loadInventory();
-
-function populatePage () {
-  var carInventory = JSON.parse(e.target.responseText)
-  console.log(carInventory)
-  // Loop over the inventory and populate the page
-  for (var i = 0; i < carInventory.length; i++) {
-    inventory += `
-        <div>${carInventory[i]}</div>
-        `
-    document.querySelector("#inventory").innerHTML = inventory
-  }
-  // Now that the DOM is loaded, establish all the event listeners needed
-
-   activateEvents();
-}
-
-function activateEvents () {
-
-}
+var inventory;
+var carInventory = [];
 
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
 function loadInventory () {
    var inventoryLoader = new XMLHttpRequest();
-   inventoryLoader.addEventListener("load", function (event) {
-      console.log("car inventory: ", event.target.responseText)
-  });
+   inventoryLoader.addEventListener("load", populatePage);
    inventoryLoader.open("GET", 'inventory.json');
    inventoryLoader.send();
-   populatePage
+   console.log()
 }
 
- loadInventory
+// Loop over the inventory and populate the page
+function populatePage (e) {
+  var carInventory = JSON.parse(e.target.responseText);
+  console.log(carInventory);
+  // for (var i = 0; i < carInventory.cars.length; i++) {
+    // document.querySelector('#inventory').innerHTML +=
+    //     `
+    //      <div>${carInventory.cars[i].make}</div>
+    //     `
+  }
+  // document.querySelector('#inventory').innerHTML = inventory
+}
+
+loadInventory();
+
+// Now that the DOM is loaded, establish all the event listeners needed
+function activateEvents () {
+
+}
